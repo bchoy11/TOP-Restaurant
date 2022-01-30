268f00867158c725f6f5ce6dab37f791c6ebd6f7
Brandon Choy <91912156+bchoy11@users.noreply.github.com>
2022-01-30 12:07:05 -0600

    contact page functional

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,10 +3,12 @@ import mailIcon from '../img/mail-icon.png';
 export default function contact(){
     const contact = document.createElement('div');
     const title = document.createElement('h1');
-    const form = document.createElement('form');
-    const input = document.createElement('div');
+    const input = document.createElement('form');
+    // const nameLabel = document.createElement('label');
+    // const emailLabel = document.createElement('email');
     const name = document.createElement('input');
     const email = document.createElement('input');
+    const submit = document.createElement('input');
     const mail = new Image();
 
     contact.id = 'contact';
@@ -20,20 +22,33 @@ export default function contact(){
     mail.className = 'icon';
 
     name.type = 'text';
+    name.id = 'name';
     name.placeholder = 'Bob A.'
 
     email.type = 'email';
+    email.id = 'email';
     email.placeholder = 'C-Yu@koalatea.com'
 
+    submit.type = 'submit';
+    submit.id = 'contact-submit';
+    submit.value = 'Send';
+
     input.id = 'contact-input';
+    input.onsubmit = submitFunction;
 
     input.appendChild(name);
     input.appendChild(email);
+    input.appendChild(submit);
 
     contact.appendChild(title);
     contact.appendChild(mail);
-    contact.appendChild(form);
     contact.appendChild(input);
 
     return contact;
+}
+
+function submitFunction(){
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    alert("Info submitted!");
 }
